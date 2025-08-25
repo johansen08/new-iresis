6c10f49f471cd774be7f98e6e4d0965adcd3cc20
dodisukmana <v.dodi.sukmana@adira.co.id>
2025-08-25 19:16:41 +0700

    Dodi | Adjust Reprint Receipt Noresi Unique

--- application/models/Receipt_reprint_fcd.php
+++ application/models/Receipt_reprint_fcd.php
@@ -6,32 +6,28 @@ class Receipt_reprint_fcd extends CI_Model
 
     function save($receipt, $user)
     {
-        $receipt = $this->db->get_where('tblprintresi', ['noresi' => $receipt['noresi']])->result_array();
-        if (empty($receipt)) {
+        $receipt_data = $this->db->get_where('tblprintresi', ['noresi' => $receipt['noresi']])->row_array();
+        if (empty($receipt_data)) {
             return ['error' => TRUE, 'code' => 400, 'message' => 'Nomor resi tidak ditemukan'];
         }
 
-        $id_resi_list = array_column($receipt, 'id_printresi');
-
-        $this->db->where_in('id_resi', $id_resi_list);
-        $receipt_reprint_exist = $this->db->get('tblresiambilbarang')->result_array();
-        if (count($receipt_reprint_exist) > 0) {
+        $this->db->where('id_resi', $receipt_data['id_printresi']);
+        $receipt_reprint_exist = $this->db->get('tblresiambilbarang')->row_array();
+        if (!empty($receipt_reprint_exist)) {
             return ['error' => TRUE, 'code' => 400, 'message' => 'Nomor resi sudah diambil. Silakan Cek data'];
         }
 
-        foreach ($receipt as $row) {
-            $insert_batch_data[] = [
-                'id_resi' => $row['id_printresi'],
-                'tanggal_resiprintulang' => date('Y-m-d H:i:s'),
-                'admin_pegawai' => $user['id_user'],
-                'nama_komputer' => $user['nama_komputer']
-            ];
-        }
+        $insert_data = [
+            'id_resi' => $receipt_data['id_printresi'],
+            'tanggal_resiprintulang' => date('Y-m-d H:i:s'),
+            'admin_pegawai' => $user['id_user'],
+            'nama_komputer' => $user['nama_komputer']
+        ];
 
-        $this->db->insert_batch('tblresiprintulang', $insert_batch_data);
+        $this->db->insert('tblresiprintulang', $insert_data);
 
         $receipt_reprint['id_resiprintulang'] = $this->db->insert_id();
-        $receipt_reprint['affected_rows'] = count($insert_batch_data);
+        $receipt_reprint['affected_rows'] = 1;
 
         return $receipt_reprint;
     }
