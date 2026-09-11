494215194dc06b20aa10b81d3db8d02709756a42
Iresis Beverra <iresisbeverra@gmail.com>
2026-09-11 11:38:11 +0700

    fix(system): kompatibilitas CodeIgniter 3 dengan PHP 8.2
    Menghilangkan deprecation warning PHP 8.2 pada core CI3:
    - #[AllowDynamicProperties] di Controller, Loader, Router, URI, DB_driver
    - Deklarasi eksplisit properti $failover di CI_DB_driver
    - #[ReturnTypeWillChange] pada SessionHandler di Session_files_driver
    
    Patch vendor: perlu diterapkan ulang bila CI3 di-upgrade.

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -50,8 +50,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/user_guide/database/
  */
-abstract class CI_DB_driver {
+#[\AllowDynamicProperties]
 
+abstract class CI_DB_driver {
+	public $failover;
 	/**
 	 * Data Source Name / Connect string
 	 *
