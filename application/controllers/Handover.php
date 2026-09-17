6ac41d14bcfa31cf7bf03d0fb3887c2f369d6ffd
Iresis Beverra <iresisbeverra@gmail.com>
2026-09-17 09:58:10 +0700

    chore(kode-mati): hapus 13 controller, 5 model, 19 view, dompdf, dan aset yang tidak dirujuk
    Hasil audit 17 Sep 2026: tidak ada satu pun yang terhubung ke menu DB,
    routes.php, view, atau JS -- sebagian bahkan sudah fatal kalau diakses.
    
    Fitur utuh yang dibuang (controller + model + view):
    - Email blast (bawaan template 2020): Email_blast, Bgprocess (controller +
      library), Email_blast_fcd, 5 view email_blast_*, 3 view email/*. View
      dipanggil sebagai email_blast/index padahal filenya email_blast_index.php;
      folder email_blast_template dan library notif tidak pernah ada.
    - Webhook (model order_fcd & konstanta JUBELIO_WEBHOOK_* tidak ada),
      Verification (verification_fcd tidak ada), Location (view tidak ada),
      Report_production_team (digantikan Report::production_team_report),
      Logaktivitas + Logaktivitas_fcd + 2 view, Sales_order + Sales_order_fcd +
      1 view, Performance_tracking + view + hook (enable_hooks FALSE),
      Operational_dashboard + view.
    - Migrate & Db_check: extends CI_Controller tanpa pengecekan login;
      /migrate menjalankan ALTER TABLE ke siapa pun yang membuka URL-nya.
    
    Kode mati di controller hidup:
    - Sku: index/edit/resync/do_resync/delete/get_data/get_sku_location_stock/
      save_sku_location_stock + view sku_index & sku_resync. Yang tersisa hanya
      upload_sku*, get_progress_file, stock_terupdate, get_stock_data.
    - Handover: print/print_preview (memanggil view handover/print yang tidak
      ada) + view scan_print_handover & template_report/handover_print.
    - Kpi_reports: view kpi_reports/index.php (placeholder) & kpi_dashboard.php.
    - 8 route yang menunjuk method yang tidak ada (packer/masalah-packer dkk,
      report/delivery-report-tab0, kpi/dashboard, purchasing/update_proses_perbaiki).
    
    Duplikat / tidak dirujuk:
    - User_fcd copy.php, Status_performa_fcd.php (controllernya pakai Kpi_fcd),
      receipt/upload_sku.php (yang dipakai sku/upload_sku), packer/scan_nosubmit.php
      (yang dipakai scan_packer_nonsubmit), libraries/dompdf (11 MB) + Pdfgenerator
      yang tidak pernah di-load, assets img/lama.png (1,7 MB), img/bg-iresis.png,
      audio/error.wav, audio/soundjnt.mp3, js/plugins/simple-plain-checkbox-tree,
      check_roleaccess.sql (0 byte).
    
    Dipertahankan walau tidak ada di menu karena berfungsi: Status_performa
    (master data) dan Picker_performance.

--- application/controllers/Handover.php
+++ application/controllers/Handover.php
@@ -9,7 +9,6 @@ class Handover extends MY_Controller
 		parent::__construct();
 
 		$this->load->model('handover_fcd');
-		$this->load->model('courrier_fcd');
 	}
 
 	public function scan_handover()
@@ -106,40 +105,4 @@ class Handover extends MY_Controller
         echo json_encode($output);
         exit();
     }
-
-	public function print()
-	{
-		$data['list_courrier'] = $this->courrier_fcd->get_courrier()->result_array();
-
-		$this->show($data);
-	}
-
-	public function print_preview()
-	{
-		ini_set('memory_limit', '-1');
-
-		$reportrange = $this->input->post('reportrange');
-
-		$id_kurir = $this->input->post('id_kurir');
-		$start_date = explode(' - ', $reportrange)[0];
-		$end_date = explode(' - ', $reportrange)[1];
-
-		$start_date_parse = date('Y-m-d H:i:s', strtotime($start_date));
-		$end_date_parse = date('Y-m-d H:i:s', strtotime($end_date));
-
-		$data['list_receipt'] = $this->handover_fcd->get_data_print($id_kurir, $start_date_parse, $end_date_parse)->result_array();
-
-		$this->load->model('courrier_fcd');
-
-		$courrier = $this->courrier_fcd->get_courrier($id_kurir)->row_array();
-
-		$data['nama_kurir'] = $courrier['nama_kurir'];
-		$data['start_date'] = $start_date;
-		$data['end_date'] = $end_date;
-
-		header("Content-type: application/vnd-ms-excel");
-		header("Content-Disposition: attachment; filename=Laporan_Resi_Keluar_Cetak_Serah_Terima-" . $data['nama_kurir'] . ".xls");
-
-		$this->load->view('template_report/handover_print', $data);
-	}
 }
